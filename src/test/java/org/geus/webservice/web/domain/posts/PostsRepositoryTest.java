3db2c5d07b7388d2e241d1c94575064015e7bb6c
geusg <gusgod@gmail.com>
2022-04-08 22:34:04 +0900

    JPA 적용

---
+++ src/test/java/org/geus/webservice/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,37 @@
+package org.geus.webservice.web.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "제목";
+        String content = "본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("geusgod@gmail.com")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}

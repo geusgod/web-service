4ebc98106f6e2eceecdb6ac68e02f690396b7aea
geusg <gusgod@gmail.com>
2022-04-08 21:32:59 +0900

    dto 적용

---
+++ src/test/java/org/geus/webservice/web/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package org.geus.webservice.web;
+
+import org.assertj.core.api.Assertions;
+import org.geus.webservice.web.dto.HelloResponseDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloControllerTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        Assertions.assertThat(dto.getName()).isEqualTo(name);
+        Assertions.assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
